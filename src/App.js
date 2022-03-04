21bd402b7b52a42ef5b60d9fc828e57836cd8523
Michel Pinto <michabbe9@live.com>
2022-03-04 17:41:13 +0100

    simple css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+    const [state, setState] = useState();
+
     return (
         <div className="App">
-            <h1>Gm World</h1>
+            <nav className="nav">
+                <h1>Nft collection</h1>
+                <button className="btn-connect">Connect wallet</button>
+            </nav>
+
+            <div className="content">
+                <h1>My nft collection</h1>
+                <h3>Each unique. Rare. Ready to moon! 🚀</h3>
+                <button>Mint</button>
+            </div>
         </div>
     );
 }
